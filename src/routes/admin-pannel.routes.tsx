d9d634740feda9419d6630eee78433de32f607ed
Sajjad Seifi <s.09334497255@gmail.com>
2021-12-07 00:25:14 +0330

    add (feacher) : form-wizard to admin routings

--- src/routes/admin-pannel.routes.tsx
+++ src/routes/admin-pannel.routes.tsx
@@ -1,5 +1,6 @@
 import { NOT_FOUND_PAGE } from '@src/constants/path'
 import { AdminLayout } from '@src/layout'
+import { FormWizardAdminPage } from '@src/pages/admin/form-wizard-admin-page'
 import React, { FC } from 'react'
 import { Redirect, Route, Switch } from 'react-router-dom'
 
@@ -7,7 +8,7 @@ export const AdminPannelRoutes: FC = () => {
   return (
     <AdminLayout>
       <Switch>
-        <Route path="/form-wizard" component={() => <></>} />
+        <Route path="/admin/form-wizard" component={FormWizardAdminPage} />
         <Redirect to={NOT_FOUND_PAGE} />
       </Switch>
     </AdminLayout>
