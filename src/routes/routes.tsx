db9a68fb96238dc37a26bac181e1be205be36f16
Sajjad Seifi <s.09334497255@gmail.com>
2021-12-07 00:15:01 +0330

    add (feacher) : admin pannel in app routing

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -2,12 +2,14 @@ import React, { FC } from 'react'
 import { Switch, Route } from 'react-router-dom'
 import { NotFound404Page } from '../pages'
 import { PublicRoutes } from '.'
+import { ADMIN_PAGE, NOT_FOUND_PAGE, PUBLIC_PAGE } from '@src/constants/path'
 
 export const Routes: FC = () => {
   return (
     <Switch>
-      <Route exact path="/404" component={NotFound404Page} />
-      <Route path="/" component={PublicRoutes} />
+      <Route exact path={NOT_FOUND_PAGE} component={NotFound404Page} />
+      <Route exact path={ADMIN_PAGE} component={NotFound404Page} />
+      <Route path={PUBLIC_PAGE} component={PublicRoutes} />
     </Switch>
   )
 }
