b117923c1b825d2ff01449c4983e8da1e779367b
Sajjad Seifi <s.09334497255@gmail.com>
2021-12-07 13:33:15 +0330

    build (section) : header navigation admin pane

---
+++ src/components/navigation/components/head-profile.tsx
@@ -0,0 +1,17 @@
+import { UserLogged } from '@src/types/user.type'
+import React, { FC } from 'react'
+import classes from './components.module.css'
+
+export const HeadProfile: FC<UserLogged> = ({ username, picture, displayRole }) => {
+  return (
+    <div className={classes.HeadProfile}>
+      <div className={classes.Pic}>
+        <img src={picture} alt="" />
+      </div>
+      <div className={classes.Captions}>
+        <span className={classes.Uname}>{username}</span>
+        <span className={classes.Roledisplay}>{displayRole}</span>
+      </div>
+    </div>
+  )
+}
--- src/components/navigation/navigation-admin.tsx
+++ src/components/navigation/navigation-admin.tsx
@@ -1,5 +1,22 @@
 import React from 'react'
-
+import classes from './navigation.module.css'
+import logo from '@src/assets/images/icons/tpn-logo.png'
+import { HiMenuAlt3 } from 'react-icons/hi'
+import { BLACK, WHITE } from '@src/constants/colors'
+import { HeadProfile } from './components'
+import { user } from '@src/constants/dumy-data'
 export const NavigationAdmin = () => {
-  return <div>NavigationAdmin</div>
+  return (
+    <div className={classes.Nav}>
+      <div className={classes.IconMenu}>
+        <HiMenuAlt3 size={26} className={classes.Icon} color={BLACK} />
+      </div>
+      <div className={classes.Content}>
+        <HeadProfile {...user} />
+      </div>
+      <a href="/" className={classes.LogoBox}>
+        <img className={classes.Logo} src={logo} alt="" />
+      </a>
+    </div>
+  )
 }
