63a4912384f64b5bb520d9f8b6ff14055bd15aed
Sajjad Seifi <s.09334497255@gmail.com>
2021-12-07 18:20:41 +0330

    add (feache) : raction creator

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,1 +1,7 @@
+//updating origin object with new target object
 export const updateObject = (origin: any, target: any) => ({ ...origin, ...target })
+//
+export const actionCreator = (type: string, payload: any) => ({
+  type,
+  payload
+})
