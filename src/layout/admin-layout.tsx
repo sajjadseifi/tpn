183fac0b2a9b9045600e1513ea22f39d4e70a2a1
Sajjad Seifi <s.09334497255@gmail.com>
2021-12-08 01:39:32 +0330

    add (feacher) : ghost & expand sidebar with redux

--- src/layout/admin-layout.tsx
+++ src/layout/admin-layout.tsx
@@ -1,12 +1,21 @@
+import React, { FC } from 'react'
 import { NavigationAdmin } from '@src/components/navigation'
 import { SidebarAdmin } from '@src/components/sidebar'
-import React, { FC, ReactPropTypes, useEffect, useState } from 'react'
+import { useSelector } from 'react-redux'
+import { RootState } from '@src/store'
+import { ISideBarState } from '@src/store/reducers/sidebar-reducer'
 
 interface AdminLayoutProps {
   children: React.ReactNode
 }
 
 export const AdminLayout: FC<AdminLayoutProps> = ({ children }) => {
+  const { expanded, ghost } = useSelector<RootState>((state) => state.sidebar) as ISideBarState
+  console.log(expanded, ghost)
+  let classes = ['layout-sidebar']
+  if (ghost) classes.push('railroad')
+
+  classes.push(expanded ? 'open' : 'close')
   return (
     <div className="layout">
       <div className="layout-content">
@@ -16,7 +25,7 @@ export const AdminLayout: FC<AdminLayoutProps> = ({ children }) => {
         <section className="layout-section">{children}</section>
       </div>
 
-      <div className={'layout-sidebar railroad'}>
+      <div className={classes.join(' ')}>
         <SidebarAdmin />
       </div>
     </div>
