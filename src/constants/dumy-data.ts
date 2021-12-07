9c1fa69e264903411e94357c1cbf78fb57291690
Sajjad Seifi <s.09334497255@gmail.com>
2021-12-07 04:10:25 +0330

    update (user type) : add uname for user

--- src/constants/dumy-data.ts
+++ src/constants/dumy-data.ts
@@ -2,6 +2,7 @@ import userPic from "@src/assets/images/static/profile.jpg";
 import { UserLogged } from "@src/types/user.type";
 
 export const user :UserLogged = {
+    username:"sajjadseifi",
     firstname:"سجاد",
     lastname:"سیفی",
     picture:userPic,
--- src/types/user.type.ts
+++ src/types/user.type.ts
@@ -1,6 +1,7 @@
 import { Roles } from "./role.type";
 
 export interface UserLogged{
+    username:string,
     firstname:string,
     lastname:string,
     picture?:string,
