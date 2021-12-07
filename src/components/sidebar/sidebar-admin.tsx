89455e5ca8a8724f3bdb67a2a2c9f90f07a6e3ec
Sajjad Seifi <s.09334497255@gmail.com>
2021-12-08 01:38:10 +0330

    add (feacher) :  click to hidden sidebar

--- src/components/sidebar/sidebar-admin.tsx
+++ src/components/sidebar/sidebar-admin.tsx
@@ -5,11 +5,16 @@ import { adminSidebarItems } from '@src/config/sidebar.config'
 import classes from './sidebar-admin.module.css'
 import { AiFillSetting as SettingIcon } from 'react-icons/ai'
 import { IoClose as CloseIcon } from 'react-icons/io5'
+import { sidebarActions } from '@src/store/actions'
+import { useDispatch } from 'react-redux'
 export const SidebarAdmin = () => {
+  const dispatch = useDispatch()
+
+  const onHiddenSidebar = () => dispatch(sidebarActions.hiddenSidebar())
   return (
     <div className={classes.Slider}>
       <div className={classes.TopSlide}>
-        <CloseIcon size={22} className={classes.Icon} onClick={() => console.log('close')} />
+        <CloseIcon size={22} className={classes.Icon} onClick={onHiddenSidebar} />
         <SettingIcon size={22} className={classes.Icon} onClick={() => console.log('setting')} />
       </div>
       <ProfileAdmin {...user} />
