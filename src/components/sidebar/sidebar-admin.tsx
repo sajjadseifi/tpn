2018b90e4c1a0a72769dfd336f6270522d50db83
Sajjad Seifi <s.09334497255@gmail.com>
2021-12-07 04:05:11 +0330

    fixed : style of sidebar

--- src/components/sidebar/sidebar-admin.tsx
+++ src/components/sidebar/sidebar-admin.tsx
@@ -1,5 +1,19 @@
 import React from 'react'
-
+import { ProfileAdmin, MenuItems } from './components'
+import { user } from '@src/constants/dumy-data'
+import { adminSidebarItems } from '@src/config/sidebar.config'
+import classes from './sidebar-admin.module.css'
+import { AiFillSetting as SettingIcon } from 'react-icons/ai'
+import { IoClose as CloseIcon } from 'react-icons/io5'
 export const SidebarAdmin = () => {
-  return <div>Sidebar Admin</div>
+  return (
+    <div className={classes.Slider}>
+      <div className={classes.TopSlide}>
+        <CloseIcon size={22} className={classes.Icon} onClick={() => console.log('close')} />
+        <SettingIcon size={22} className={classes.Icon} onClick={() => console.log('setting')} />
+      </div>
+      <ProfileAdmin {...user} />
+      <MenuItems items={adminSidebarItems} />
+    </div>
+  )
 }
