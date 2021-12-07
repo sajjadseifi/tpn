ddc9f38def027e94773df7ce0452c61662f80abe
Sajjad Seifi <s.09334497255@gmail.com>
2021-12-07 23:07:25 +0330

    add many module in directory specifier

--- src/components/dialog/index.ts
+++ src/components/dialog/index.ts
@@ -1,2 +1,4 @@
-export { Dialog } from "./dialog";
-export type { DialogPorps } from "./dialog";
+export { Dialog } from './dialog'
+export type { DialogPorps } from './dialog'
+export type { VisibilityType, StatusType } from './types-backdrop'
+export { DialogIcon } from './dialog-icon'
