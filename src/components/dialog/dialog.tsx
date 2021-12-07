bae77d864df867000a5068faf70b129c37175335
Sajjad Seifi <s.09334497255@gmail.com>
2021-12-07 23:06:44 +0330

    fixed (issue): animations and clean code

--- src/components/dialog/dialog.tsx
+++ src/components/dialog/dialog.tsx
@@ -1,21 +1,23 @@
-import React, { FC, useState } from 'react'
-import { IoAddCircle, IoAccessibilitySharp } from 'react-icons/io5'
+import React, { FC, useEffect, useState } from 'react'
+import { IoAddCircle, IoAccessibilitySharp, IoReorderFour } from 'react-icons/io5'
 import { IconBaseProps, IconType } from 'react-icons/lib'
 import { Backrop } from '../backdrop/backrop'
 import { IconButton } from '../ui/icon-button'
 import classes from './dialog.module.css'
 import * as colors from '@src/constants/colors'
-
-type StatusType = 'Info' | 'Error' | 'Success' | 'Default'
+import { DialogIcon, StatusType, VisibilityType } from '.'
+import { useDialogDispatch, useDialogSelector } from './context/hook-dialog'
+import { actionTypes } from './context'
 
 export interface DialogPorps {
   icon?: IconType
   iconProps?: IconBaseProps
-  title: string
+  title?: string
   onOk?: () => void
   onCancel?: () => void
   status?: StatusType
   children?: React.ReactNode
+  visibility?: VisibilityType
 }
 const colorStatus = {
   Info: colors.CORNFLOWER_BLUE,
@@ -23,33 +25,40 @@ const colorStatus = {
   Success: colors.CORNFLOWER_GREEN,
   Default: colors.GRAYWHITE
 }
-export const Dialog: FC<DialogPorps> = ({ onCancel, onOk, children, icon: Icon, status, iconProps, title }) => {
-  const [open, setOpen] = useState(true)
-  const [rm, setRm] = useState(false)
+const colorStatusDark = {
+  Info: colors.BLUE,
+  Error: colors.RED,
+  Success: colors.GREEN,
+  Default: colors.BLACK
+}
+export const Dialog: FC<DialogPorps> = ({}) => {
+  const { icon, title, visibility, status, children, iconProps, onOk, onCancel }: DialogPorps = useDialogSelector()
+  const dispatch = useDialogDispatch()
   let bgTop = colorStatus.Default
   if (status) bgTop = colorStatus[status]
 
-  const onAction = (act: boolean) => {
-    if (act) onOk && onOk()
-    else onCancel && onCancel()
-    setOpen(false)
+  const onAction = (fc: (() => void) | undefined) => {
+    removeOnDom()
+    fc && fc()
   }
-  const removeOnDom = () => {
-    setOpen(false)
-    setTimeout(() => setRm(true), 1000)
-  }
-  if (rm) return null
+
+  const removeOnDom = () => dispatch(actionTypes.closeDialog())
+
+  const color = (status && colorStatusDark[status]) || colors.BLACK
+
+  if (iconProps && !iconProps.color) iconProps.color = color
+
   return (
-    <Backrop onClosed={removeOnDom} open={open}>
+    <Backrop onClosed={removeOnDom} visibility={visibility}>
       <div className={classes.Dialog}>
-        <div style={{ background: bgTop }} className={classes.Top}>
+        <div style={{ background: bgTop, color }} className={classes.Top}>
           <h3 className={classes.Title}>{title}</h3>
-          {Icon && <Icon {...iconProps} className={classes.Icon} />}
+          <DialogIcon {...{ icon, iconProps, status }} />
         </div>
         <div className={classes.Body}>{children}</div>
         <div className={classes.ButtonConaienr}>
-          <IconButton onClick={() => onAction(true)} outline color={colors.GREEN} icon={IoAddCircle} title="تایید" />
-          <IconButton onClick={() => onAction(false)} color={colors.RED} icon={IoAccessibilitySharp} title="لغو" />
+          <IconButton onClick={() => onAction(onOk)} outline color={colors.GREEN} icon={IoAddCircle} title="تایید" />
+          <IconButton onClick={() => onAction(onCancel)} color={colors.RED} icon={IoAccessibilitySharp} title="لغو" />
         </div>
       </div>
     </Backrop>
@@ -59,5 +68,6 @@ export const Dialog: FC<DialogPorps> = ({ onCancel, onOk, children, icon: Icon, 
 Dialog.defaultProps = {
   onOk: () => {},
   onCancel: () => {},
-  status: 'Default'
+  status: 'Default',
+  visibility: 'Start'
 }
