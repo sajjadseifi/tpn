5fef02d3469b5a7fe9f6f53f462b8cd6ddeb8961
Sajjad Seifi <s.09334497255@gmail.com>
2021-12-08 00:50:04 +0330

    add(feacher): fucntion type recuer & nullable type

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,3 +1,8 @@
-import * as colors from "@src/constants/colors";
+import * as colors from '@src/constants/colors'
 
 export type ColorType = keyof typeof colors
+
+//redux type
+export type FCR<StateType, ActionType> = (state: StateType | undefined, action: ActionType) => StateType
+//nullable of T
+export type Partial<T> = { [P in keyof T]?: T[P] }
