cb87bc86a8dea55b7d3a1ebeb7ac4f4ca2cd6df3
Sajjad Seifi <s.09334497255@gmail.com>
2021-12-07 23:05:10 +0330

    fixed (issue): change open to 3 type visibility

--- src/components/backdrop/backrop.tsx
+++ src/components/backdrop/backrop.tsx
@@ -1,32 +1,32 @@
 import React, { FC, useEffect, useState } from 'react'
+import { VisibilityType } from '../dialog'
+import { actionTypes } from '../dialog/context'
+import { useDialogDispatch } from '../dialog/context/hook-dialog'
 import classes from './backdrop.module.css'
 interface BackropProps {
   bg?: string
   justifyContent?: string
   alignItems?: string
   children?: React.ReactNode
-  open?: boolean
+  visibility?: VisibilityType
   closeable?: boolean
   onClosed?: () => void
 }
 
-export const Backrop: FC<BackropProps> = ({ open, onClosed, closeable, children, ...style }) => {
-  const [isOpen, setOpen] = useState(open)
-
-  useEffect(() => {
-    setOpen(open)
-  }, [open])
+export const Backrop: FC<BackropProps> = ({ visibility, onClosed, closeable, children, ...style }) => {
+  const dispatch = useDialogDispatch()
 
   const onClose = () => {
-    if (closeable) setOpen(false)
+    if (closeable) dispatch(actionTypes.closeDialog())
     else onClosed && onClosed()
   }
-  const activation = isOpen ? classes.Open : classes.Close
+
+  const activation = visibility && classes[visibility]
 
   return (
-    <div className={classes.Container} style={style}>
-      <div onClick={onClose} className={`${classes.BackDrop} ${activation}`}></div>
-      <div className={`${classes.ContianerChild} ${activation}`}>{children}</div>
+    <div className={`${classes.Container} ${activation}`} style={style}>
+      <div onClick={onClose} className={`${classes.BackDrop}`}></div>
+      <div className={`${classes.ContianerChild}`}>{children}</div>
     </div>
   )
 }
