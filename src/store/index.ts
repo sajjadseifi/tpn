ad91365469b6c4a5d3630d02984f7b9ac7391641
Sajjad Seifi <s.09334497255@gmail.com>
2021-12-08 01:09:56 +0330

    add sidebar reducer

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,14 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux'
+import sidebarReducer, { ISideBarState } from './reducers/sidebar-reducer'
 import thunk from 'redux-thunk'
 
-export interface RootState {}
+export interface RootState {
+  sidebar: ISideBarState
+}
 
-export const reducers = {}
+export const reducers = {
+  sidebar: sidebarReducer
+}
 
 export interface AppThunkAction<TAction> {
   (dispatch: (action: TAction) => void, getState: () => RootState): void
