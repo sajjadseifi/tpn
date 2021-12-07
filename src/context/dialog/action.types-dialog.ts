9dd86b0b0021fe0472ec1b347d731ac59cba6fa0
Sajjad Seifi <s.09334497255@gmail.com>
2021-12-07 18:25:04 +0330

    build action functions for action types

--- src/context/dialog/action.types-dialog.ts
+++ src/context/dialog/action.types-dialog.ts
@@ -1,3 +1,10 @@
-export const DIALOG_OPEN ='DIALOG_OPEN'
-export const DIALOG_CLOSE ='DIALOG_CLOSE'
-export const DIALOG_INIT ='DIALOG_INIT'
+import { actionCreator } from '@src/utils/utils'
+import { DialogStateProps } from '.'
+
+export const DIALOG_OPEN = 'DIALOG_OPEN'
+export const DIALOG_CLOSE = 'DIALOG_CLOSE'
+export const DIALOG_INIT = 'DIALOG_INIT'
+
+export const initDialog = (values: DialogStateProps) => actionCreator(DIALOG_CLOSE, { payload: values })
+export const openDialog = () => actionCreator(DIALOG_OPEN, {})
+export const closeDialog = () => actionCreator(DIALOG_CLOSE, {})
