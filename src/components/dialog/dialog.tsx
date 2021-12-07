9bafe488af043f361b48fdc2462f5aeba1d9ae3e
Sajjad Seifi <s.09334497255@gmail.com>
2021-12-07 15:03:13 +0330

    add styling and type for dialog

--- src/components/dialog/dialog.tsx
+++ src/components/dialog/dialog.tsx
@@ -1,28 +1,56 @@
-import { GREEN, RED } from '@src/constants/colors'
-import React, { FC } from 'react'
+import React, { FC, useState } from 'react'
 import { IoAddCircle, IoAccessibilitySharp } from 'react-icons/io5'
 import { IconBaseProps, IconType } from 'react-icons/lib'
 import { Backrop } from '../backdrop/backrop'
 import { IconButton } from '../ui/icon-button'
 import classes from './dialog.module.css'
+import * as colors from '@src/constants/colors'
+
+type StatusType = 'Info' | 'Error' | 'Success' | 'Default'
 
 interface DialogPorps {
   icon?: IconType
   iconProps?: IconBaseProps
+  title: string
+  onOk?: () => void
+  onCancel?: () => void
+  status?: StatusType
 }
+const colorStatus = {
+  Info: colors.CORNFLOWER_BLUE,
+  Error: colors.BISQUE,
+  Success: colors.CORNFLOWER_GREEN,
+  Default: colors.GRAYWHITE
+}
+export const Dialog: FC<DialogPorps> = ({ onCancel, onOk, children, icon: Icon, status, iconProps, title }) => {
+  const [open, setOpen] = useState(true)
+  let bgTop = colorStatus.Default
+  if (status) bgTop = colorStatus[status]
 
-export const Dialog: FC<DialogPorps> = ({ icon: Icon, iconProps, children }) => {
+  const onAction = (act: boolean) => {
+    if (act) onOk && onOk()
+    else onCancel && onCancel()
+    setOpen(false)
+  }
   return (
-    <Backrop open={true}>
-      <div className={classes.Top}>
-        <span className={classes.Title}></span>
-        {Icon && <Icon {...iconProps} />}
-      </div>
-      <div className={classes.Body}>{children}</div>
-      <div className={classes.ButtonConaienr}>
-        <IconButton outline color={GREEN} icon={IoAddCircle} title="تایید" />
-        <IconButton color={RED} icon={IoAccessibilitySharp} title="لغو" />
+    <Backrop open={open}>
+      <div className={classes.Dialog}>
+        <div style={{ background: bgTop }} className={classes.Top}>
+          <h3 className={classes.Title}>{title}</h3>
+          {Icon && <Icon {...iconProps} className={classes.Icon} />}
+        </div>
+        <div className={classes.Body}>{children}</div>
+        <div className={classes.ButtonConaienr}>
+          <IconButton onClick={() => onAction(true)} outline color={colors.GREEN} icon={IoAddCircle} title="تایید" />
+          <IconButton onClick={() => onAction(false)} color={colors.RED} icon={IoAccessibilitySharp} title="لغو" />
+        </div>
       </div>
     </Backrop>
   )
+}
+
+Dialog.defaultProps = {
+  onOk: () => {},
+  onCancel: () => {},
+  status: 'Default'
 }
