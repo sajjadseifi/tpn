dafadf59ec14fa9e00db5031192d3c7f61a20dee
Sajjad Seifi <s.09334497255@gmail.com>
2021-12-07 23:08:36 +0330

    add test click  dialog and build func for slider

--- src/components/navigation/navigation-admin.tsx
+++ src/components/navigation/navigation-admin.tsx
@@ -5,16 +5,31 @@ import { HiMenuAlt3 } from 'react-icons/hi'
 import { BLACK, WHITE } from '@src/constants/colors'
 import { HeadProfile } from './components'
 import { user } from '@src/constants/dumy-data'
+import { actionTypes } from '../dialog/context'
+import { useDialogDispatch } from '../dialog/context/hook-dialog'
 export const NavigationAdmin = () => {
+  const dispatch = useDialogDispatch()
+  const onMenuClick = () => {
+    dispatch(
+      actionTypes.initDialog({
+        title: 'خطا',
+        visibility: 'Show',
+        children: <>شما فرم فلانی را پر نکردید لطفا پر کینید</>,
+        // icon: IoWomanSharp,
+        status: 'Error'
+      })
+    )
+    // dispatch(actionTypes.openDialog())
+  }
   return (
     <div className={classes.Nav}>
-      <div className={classes.IconMenu}>
+      <div onClick={onMenuClick} className={classes.IconMenu}>
         <HiMenuAlt3 size={26} className={classes.Icon} color={BLACK} />
       </div>
       <div className={classes.Content}>
         <HeadProfile {...user} />
       </div>
-      <a href="/" className={classes.LogoBox}>
+      <a className={classes.LogoBox}>
         <img className={classes.Logo} src={logo} alt="" />
       </a>
     </div>
