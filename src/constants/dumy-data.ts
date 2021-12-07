750da7375bab4acd06f2f241acfc23255bf0c940
Sajjad Seifi <s.09334497255@gmail.com>
2021-12-07 04:01:46 +0330

    build : dumy data project

---
+++ src/constants/dumy-data.ts
@@ -0,0 +1,9 @@
+import userPic from "@src/assets/images/static/profile.jpg";
+import { UserLogged } from "@src/types/user.type";
+
+export const user :UserLogged = {
+    firstname:"سجاد",
+    lastname:"سیفی",
+    picture:userPic,
+    role:"admin"
+};
