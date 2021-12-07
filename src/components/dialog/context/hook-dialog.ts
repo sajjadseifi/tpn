a0b38b51b9aea982326d2b92451b111e0d0639dd
Sajjad Seifi <s.09334497255@gmail.com>
2021-12-07 23:00:56 +0330

    build doialog context managment hook

---
+++ src/components/dialog/context/hook-dialog.ts
@@ -0,0 +1,14 @@
+import * as React from 'react'
+import { useDispatch, useSelector } from '@src/hook'
+import { DialogStateContext, DialogProvider } from '.'
+
+export const useDialog = () => {
+  const context = React.useContext(DialogStateContext)
+  if (context === undefined) throw new Error('useDialog must be used within a DialogProvider')
+
+  return context
+}
+export const useDialogDispatch = () => useDispatch(DialogStateContext, 'useDialogDispatch', DialogProvider.name)
+
+export const useDialogSelector = (cb = (state: any) => state) =>
+  useSelector(DialogStateContext, 'useDialogSelector', DialogProvider.name, cb)
