5578f1b540b5ce6a088ee5a7558433661699ce3b
Sajjad Seifi <s.09334497255@gmail.com>
2021-12-08 18:13:55 +0330

    impliment functionality of wizard form

--- src/store/reducers/wizard-redcuer.ts
+++ src/store/reducers/wizard-redcuer.ts
@@ -1,8 +1,10 @@
 import { FormContnetProps } from '@src/components/form-wizard'
 import { FCR, IDictionary } from '@src/types/types'
+import { updateObject } from '@src/utils/utils'
+import { wizardTypes } from '../types'
 import { WizardAction } from '../types/wizard-type'
 
-interface IWizard extends FormContnetProps {
+export interface IWizard extends FormContnetProps {
   render: ({ form }: { form: IDictionary<any> }) => JSX.Element
 }
 
@@ -21,12 +23,39 @@ const initialState: WizardState = {
   errors: {},
   visitedLevel: 0
 }
+const init = (state: WizardState, wizards: IWizard[]) => updateObject(state, { wizards })
 
-const authReducer: FCR<WizardState, WizardAction> = (state = initialState, action) => {
+const goPrev = (state: WizardState) => {
+  if (state.level <= 0) return state
+
+  return updateObject(state, { level: state.level - 1 })
+}
+
+const goNext = (state: WizardState) => {
+  if (state.level >= state.wizards.length) return state
+
+  return updateObject(state, { level: state.level + 1 })
+}
+
+const wizardReducer: FCR<WizardState, WizardAction> = (state = initialState, action) => {
   switch (action.type) {
+    case wizardTypes.WIZARD_INIT:
+      return init(state, action.payload)
+    case wizardTypes.WIZARD_CLEAR:
+      return initialState
+    case wizardTypes.WIZARD_NEXT:
+      return goNext(state)
+    case wizardTypes.WIZARD_PREV:
+      return goPrev(state)
+    case wizardTypes.WIZARD_SUBMITED:
+      return state
+    case wizardTypes.WIZARD_LOADING_START:
+      return updateObject(state, { loading: true })
+    case wizardTypes.WIZARD_LOADING_FINISHED:
+      return updateObject(state, { loading: false })
     default:
       return state
   }
 }
 
-export default authReducer
+export default wizardReducer
