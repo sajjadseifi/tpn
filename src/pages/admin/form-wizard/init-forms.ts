64aebbadb7c99386ab8bae454df2e6342b42bd64
Sajjad Seifi <s.09334497255@gmail.com>
2021-12-08 22:47:55 +0330

    impliment onSubmit bodies

--- src/pages/admin/form-wizard/init-forms.ts
+++ src/pages/admin/form-wizard/init-forms.ts
@@ -1,4 +1,5 @@
 import { IWizard } from '@src/store/reducers/wizard-redcuer'
+import { sleep } from '@src/utils/utils'
 import { AcceptorForm } from './sections'
 import { IdenfiyForm } from './sections'
 import { PersonalForm } from './sections'
@@ -12,7 +13,20 @@ export const forms: IWizard[] = [
       city: '',
       sex: -1
     },
-    render: PersonalForm
+    render: PersonalForm,
+    onSubmit: (values, dispatch) => {
+      console.log('start')
+      sleep(1) //fetch data
+      console.log('end')
+
+      const ok = Math.floor(Math.random() * 2) == 1
+
+      const messages = ok
+        ? ['اطلاعات شخصی شما مورد تایید قرار گرفت']
+        : ['در اطلاعات شخصی شما موردی وجود دارد لطفا آن را بررسی کنید']
+
+      return { ok, messages }
+    }
   },
   {
     title: 'اطلاعات هویتی',
@@ -21,13 +35,34 @@ export const forms: IWizard[] = [
       phone: '',
       nationalCode: ''
     },
-    render: IdenfiyForm
+    render: IdenfiyForm,
+    onSubmit: (values, dispatch) => {
+      console.log('start')
+      sleep(3) //fetch data
+      console.log('end')
+
+      const ok = Math.floor(Math.random() * 2) == 1
+
+      const messages = ok ? ['ایمیل یا پیامک برای شما ارسال شد'] : ['لطفا ایمیل و شماره تلفن را درست وارد کیند']
+
+      return { ok, messages }
+    }
   },
   {
     title: 'تایید هویت',
     form: {
       code: ''
     },
-    render: AcceptorForm
+    render: AcceptorForm,
+    onSubmit: (values, dispatch) => {
+      console.log('start')
+      sleep(0.5) //fetch data
+      console.log('end')
+
+      const ok = Math.floor(Math.random() * 2) == 1
+
+      const messages = ok ? ['ممنون از به تایید رساندن حسابتان'] : ['کد وارد شده صحیح نمیباشد']
+      return { ok, messages }
+    }
   }
 ]
