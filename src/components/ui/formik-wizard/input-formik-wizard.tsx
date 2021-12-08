a8139bb3969dd26c2b92ca540b1ac2abb201111a
Sajjad Seifi <s.09334497255@gmail.com>
2021-12-09 03:25:00 +0330

    fixed (issues) :change body of input

--- src/components/ui/formik-wizard/input-formik-wizard.tsx
+++ src/components/ui/formik-wizard/input-formik-wizard.tsx
@@ -1,11 +1,29 @@
 import React, { FC } from 'react'
+import { Field, FieldAttributes } from 'formik'
+import { classes, ErrorFormikWizard } from '.'
+interface InputFormikWizardProps extends FieldAttributes<any> {
+  name: string
+  label: string
+  type?: string
+  error?: string
+  touched?: string
+}
 
-interface InputFormikWizardProps {}
-const InputFormikWizard: FC<InputFormikWizardProps> = ({}) => {
+export const InputFormikWizard: FC<InputFormikWizardProps> = ({ label, name, type }) => {
   return (
-    <div>
-      <label htmlFor=""></label>
-      <input type="text" name="" id="" />
+    <div className={classes.InputWizard}>
+      <div className={classes.InputContainer}>
+        <Field className={classes.Input} id={name} {...{ name, type }} required />
+        <h6 className={classes.Label}>
+          <label htmlFor={name}>{label}</label>
+        </h6>
+        <div className={classes.InputProgress}></div>
+      </div>
+      <ErrorFormikWizard {...{ name }} />
     </div>
   )
+}
+
+InputFormikWizard.defaultProps = {
+  type: 'text'
 }
