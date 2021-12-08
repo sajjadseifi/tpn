d71a7b063bc1b89db6baa8b3a4e21f10b40c8d84
Sajjad Seifi <s.09334497255@gmail.com>
2021-12-08 16:59:03 +0330

    fixed new design of form wizrd and init form props

--- src/components/form-wizard/form-wizard.tsx
+++ src/components/form-wizard/form-wizard.tsx
@@ -1,22 +1,43 @@
-import React, { FC, ReactPropTypes } from 'react'
+import React, { FC, Fragment } from 'react'
 import classes from './form-wizard.module.css'
 import { useSelector } from 'react-redux'
 import { RootState } from '@src/store'
 import { WizardState } from '@src/store/reducers/wizard-redcuer'
-import { MovmentFormWizard } from './movment-form-wizard'
+import Proggres from './progress'
+import { IDictionary } from '@src/types/types'
+import { Form, Formik, FormikHelpers } from 'formik'
+import { TitleUi } from '../ui'
 
-interface IWizard {
+export type FormType = IDictionary<any>
+
+export interface FormContnetProps {
   title: string
-
-  render: (props: any) => JSX.Element
+  form: FormType
+  movment?: FC<any>
+  onSubmit?: <T>(values: T, { setSubmitting }: FormikHelpers<T>) => void
 }
 
 export const FromWizard = () => {
-  const {} = useSelector<RootState>((state) => state.wizard) as WizardState
+  const { level, wizards } = useSelector<RootState>((state) => state.wizard) as WizardState
+  const WIZARD = wizards[level]
 
+  if (!WIZARD) return <Fragment></Fragment>
+
+  const { form, render: Render, title, movment: Movment, onSubmit: submited } = wizards[level]
+
+  const onSubmit = submited ? submited : () => {}
   return (
     <div>
-      <MovmentFormWizard />
+      <Proggres />
+      <div className={classes.FormContent}>
+        <TitleUi className={classes.Title} subject={title} />
+        <Formik initialValues={form} {...{ onSubmit }}>
+          <Form>
+            <Render {...{ form }} />
+            {Movment && <Movment />}
+          </Form>
+        </Formik>
+      </div>
     </div>
   )
 }
