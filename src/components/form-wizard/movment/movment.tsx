d0c8a1541f658bdce0166bd2f94708026552a993
Sajjad Seifi <s.09334497255@gmail.com>
2021-12-08 22:46:08 +0330

    use formik formsubmit to send up level

--- src/components/form-wizard/movment/movment.tsx
+++ src/components/form-wizard/movment/movment.tsx
@@ -1,26 +1,31 @@
-import React from 'react'
+import React, { FC } from 'react'
 import { RootState } from '@src/store'
 import { WizardState } from '@src/store/reducers/wizard-redcuer'
 import { useDispatch, useSelector } from 'react-redux'
 import { classes, nextBtn, prevBtn, submitedBtn } from '.'
 import { IconButton } from '@src/components/ui'
 import { wizardActions } from '@src/store/actions'
+import { Partial } from '@src/types/types'
 
-export const Movment = () => {
+interface MovmentProps {
+  submitForm: (() => Promise<void>) & (() => Promise<any>)
+  loading: boolean
+}
+
+export const Movment: FC<Partial<MovmentProps>> = ({ submitForm, loading }) => {
   const dispatch = useDispatch()
-  const { level, wizards, isLoading } = useSelector<RootState>((state) => state.wizard) as WizardState
+  const { level, wizards } = useSelector<RootState>((state) => state.wizard) as WizardState
   const isStart = 0 === level
   const isEnd = wizards.length - 1 == level
 
-  const onNext = () => dispatch(wizardActions.nextWizard())
   const onPrev = () => dispatch(wizardActions.prevWizard())
-  const onSubmit = () => dispatch(wizardActions.submitedWizard())
+  // const onNext = () => dispatch(wizardActions.nextWizard())
+  // const onSubmit = () => dispatch(wizardActions.submitedWizard())
   return (
     <div className={classes.Movment}>
       {isStart && <div></div>}
       {!isStart && <IconButton onClick={onPrev} {...prevBtn} />}
-      {!isEnd && <IconButton onClick={onNext} {...nextBtn} />}
-      {isEnd && <IconButton onClick={onSubmit} {...submitedBtn} />}
+      {!isEnd && <IconButton onClick={submitForm} {...(isEnd ? submitedBtn : nextBtn)} />}
     </div>
   )
 }
