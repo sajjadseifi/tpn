e457f35301a8ce01862ba01b8fbb6074aac3e270
Sajjad Seifi <s.09334497255@gmail.com>
2021-12-08 21:00:31 +0330

    add select enterd level to form wizard

--- src/components/form-wizard/form-wizard.tsx
+++ src/components/form-wizard/form-wizard.tsx
@@ -1,6 +1,6 @@
 import React, { FC, Fragment } from 'react'
 import classes from './form-wizard.module.css'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { RootState } from '@src/store'
 import { WizardState } from '@src/store/reducers/wizard-redcuer'
 import Proggres from './progress'
@@ -8,6 +8,7 @@ import { IDictionary } from '@src/types/types'
 import { Form, Formik, FormikHelpers } from 'formik'
 import { TitleUi } from '../ui'
 import { Movment } from './movment'
+import { wizardActions } from '@src/store/actions'
 
 export type FormType = IDictionary<any>
 
@@ -19,6 +20,7 @@ export interface FormContnetProps {
 }
 
 export const FromWizard = () => {
+  const dispatch = useDispatch()
   const { level, wizards } = useSelector<RootState>((state) => state.wizard) as WizardState
   const WIZARD = wizards[level]
 
@@ -30,7 +32,7 @@ export const FromWizard = () => {
   const MovmentCmp = movment ? movment : Movment
   return (
     <div>
-      <Proggres />
+      <Proggres clickedProggresItem={(entered: number) => dispatch(wizardActions.selectEnteredWizard(entered))} />
       <div className={classes.FormContent}>
         <TitleUi className={classes.Title} subject={title} />
         <Formik initialValues={form} {...{ onSubmit }}>
