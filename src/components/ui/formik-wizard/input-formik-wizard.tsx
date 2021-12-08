edfef787d66b0f0b90254a451010ffd0e678ff72
Sajjad Seifi <s.09334497255@gmail.com>
2021-12-08 23:19:27 +0330

    build base of input form wizard

---
+++ src/components/ui/formik-wizard/input-formik-wizard.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from 'react'
+
+interface InputFormikWizardProps {}
+const InputFormikWizard: FC<InputFormikWizardProps> = ({}) => {
+  return (
+    <div>
+      <label htmlFor=""></label>
+      <input type="text" name="" id="" />
+    </div>
+  )
+}
