80a1fd68425d18b091ec8d73920af580e251c3a9
Sajjad Seifi <s.09334497255@gmail.com>
2021-12-09 03:24:12 +0330

    exported all formwizard ui

---
+++ src/components/ui/formik-wizard/index.ts
@@ -0,0 +1,4 @@
+import classes from './form-wizard.module.css'
+export { InputFormikWizard } from './input-formik-wizard'
+export { ErrorFormikWizard } from './error-formik-wizard'
+export { classes }
