8987e0c73c52f303e834da7c880a78e87edd95e9
Sajjad Seifi <s.09334497255@gmail.com>
2021-12-08 17:06:11 +0330

    add (init , clear) type

--- src/store/types/wizard-type.ts
+++ src/store/types/wizard-type.ts
@@ -1,20 +1,43 @@
 import { AppThunkAction } from '..'
 
+export const WIZARD_INIT = '[WIZARD_INIT]'
+export const WIZARD_CLEAR = '[WIZARD_CLEAR]'
 export const WIZARD_NEXT = '[WIZARD_NEXT]'
 export const WIZARD_PREV = '[WIZARD_PREV]'
 export const WIZARD_SUBMITED = '[WIZARD_SUBMITED]'
 export const WIZARD_LOADING_START = '[WIZARD_LOADING_START]'
 export const WIZARD_LOADING_FINISHED = '[WIZARD_LOADING_FINISHED]'
 
+export interface InitWizardAction {
+  type: typeof WIZARD_LOADING_START
+}
+export interface ClearWizardAction {
+  type: typeof WIZARD_LOADING_FINISHED
+}
 export interface NextWizardAction {
   type: typeof WIZARD_NEXT
 }
 export interface PrevWizardAction {
-  type: typeof WIZARD_NEXT
+  type: typeof WIZARD_PREV
 }
 export interface SubmitedWizardAction {
-  type: typeof WIZARD_NEXT
+  type: typeof WIZARD_SUBMITED
 }
-export type WizardAction = NextWizardAction | PrevWizardAction | SubmitedWizardAction
+
+export interface LoadingStartWizardAction {
+  type: typeof WIZARD_LOADING_START
+}
+export interface LoadingFinishedWizardAction {
+  type: typeof WIZARD_LOADING_FINISHED
+}
+
+export type WizardAction =
+  | InitWizardAction
+  | ClearWizardAction
+  | NextWizardAction
+  | PrevWizardAction
+  | SubmitedWizardAction
+  | LoadingStartWizardAction
+  | LoadingFinishedWizardAction
 
 export type AuthThunk = AppThunkAction<WizardAction>
