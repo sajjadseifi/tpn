d31adcc2e0ac88c13fd9a175a164730417170b49
Sajjad Seifi <s.09334497255@gmail.com>
2021-12-08 22:44:56 +0330

    build sleep function

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -5,3 +5,6 @@ export const actionCreator = (type: string, payload?: any) => ({
   type,
   payload
 })
+
+//
+export const sleep = (second: number) => new Promise((res, rej) => setTimeout(() => res(0), second * 1000))
