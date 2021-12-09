bc6852a472926fcd914337dee35c5b0036d4e5c6
Sajjad Seifi <s.09334497255@gmail.com>
2021-12-09 17:34:54 +0330

    add (feacher) exact path to nav-item

--- src/components/navigation/home/components/nav-item.tsx
+++ src/components/navigation/home/components/nav-item.tsx
@@ -5,18 +5,22 @@ import { classes } from '.'
 export interface NavItemHomeNavigationProps {
   route: string
   title: string
+  exact?: boolean
 }
 export const NavItemHomeNavigation: FC<NavItemHomeNavigationProps> = ({
   route: to,
-  title
+  title,
+  exact
 }) => {
   return (
     <li className={classes.NavItem}>
       <NavLink
-        {...{ title, to }}
+        {...{ to, exact }}
         className={classes.NavLink}
         activeClassName={classes.NavLinkActive}
-      />
+      >
+        {title}
+      </NavLink>
     </li>
   )
 }
