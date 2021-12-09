fea60a61cddfa53138998068a0c3d7b69dc4fa22
Sajjad Seifi <s.09334497255@gmail.com>
2021-12-09 18:38:24 +0330

    inject PresentTpn on home page

--- src/pages/public/home/home-page.tsx
+++ src/pages/public/home/home-page.tsx
@@ -2,12 +2,13 @@ import React from 'react'
 import { classes } from '.'
 import homePagePic from '@src/assets/images/static/home-page-2.svg'
 import { HomeNavigation } from '@src/components/navigation'
+import { PresentTpn } from './components'
 export const HomePage = () => {
   return (
     <div className={classes.HomePage}>
       <HomeNavigation />
       <section>
-        <div>...</div>
+        <PresentTpn />
         <div className={classes.HomePicContainer}>
           <img className={classes.HomePic} src={homePagePic} alt="" />
         </div>
