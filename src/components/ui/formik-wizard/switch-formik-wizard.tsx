448bcf7b5f7a16b2792d2de10f52ff90a574150b
Sajjad Seifi <s.09334497255@gmail.com>
2021-12-09 05:36:28 +0330

    build Swich form wizrad

---
+++ src/components/ui/formik-wizard/switch-formik-wizard.tsx
@@ -0,0 +1,31 @@
+import { Field } from 'formik'
+import React, { FC } from 'react'
+import { classes, ErrorFormikWizard } from '.'
+import { TitleFormWizard } from './title-form-wizard'
+
+export type SwitchItem<T> = { display: string; value: T }
+interface SwitchFormikWizardProps {
+  name: string
+  label: string
+  type?: 'checked' | 'radio'
+  items: SwitchItem<any>[]
+}
+
+export const SwitchFormikWizard: FC<SwitchFormikWizardProps> = ({ label, name, type, items }) => {
+  return (
+    <div className={classes.Switch}>
+      <TitleFormWizard {...{ label, name }} />
+      {items.map(({ display, value }) => (
+        <label className={classes.SwitchItem}>
+          <Field {...{ name, type, value }} />
+          <span>{display}</span>
+        </label>
+      ))}
+      <ErrorFormikWizard {...{ name }} />
+    </div>
+  )
+}
+
+SwitchFormikWizard.defaultProps = {
+  type: 'radio'
+}
