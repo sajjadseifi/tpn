7cd03b8bd31c842048178326407024a4a4d9f576
Sajjad Seifi <s.09334497255@gmail.com>
2021-12-09 05:40:04 +0330

    add switch sex section to form

--- src/pages/admin/form-wizard/sections/pesonal-form.tsx
+++ src/pages/admin/form-wizard/sections/pesonal-form.tsx
@@ -1,5 +1,5 @@
-import { InputFormikWizard } from '@src/components/ui/formik-wizard'
 import React from 'react'
+import { InputFormikWizard, SwitchFormikWizard, SwitchItem } from '@src/components/ui/formik-wizard'
 import * as Yup from 'yup'
 
 const withMinMaxReq = () =>
@@ -13,6 +13,10 @@ export const personalSchema = Yup.object().shape({
   city: withMinMaxReq(),
   sex: Yup.number().min(0, '').max(1, '')
 })
+const sexSwitchs: SwitchItem<any>[] = [
+  { display: 'مرد', value: '0' },
+  { display: 'زن', value: '1' }
+]
 
 export const PersonalForm = () => {
   return (
@@ -20,6 +24,7 @@ export const PersonalForm = () => {
       <InputFormikWizard label="نام" name="firstname" />
       <InputFormikWizard label="نام خانوادگی" name="lastname" />
       <InputFormikWizard label="شهر" name="city" />
+      <SwitchFormikWizard label="جنسیت" name="sex" items={sexSwitchs} />
     </>
   )
 }
