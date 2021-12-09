b137e023a5b799bc80f3a05ffdc92008bd48f0f3
Sajjad Seifi <s.09334497255@gmail.com>
2021-12-09 05:36:53 +0330

    export switch form wizard

--- src/components/ui/formik-wizard/index.ts
+++ src/components/ui/formik-wizard/index.ts
@@ -2,4 +2,5 @@ import classes from './form-wizard.module.css'
 export { InputFormikWizard } from './input-formik-wizard'
 export { ErrorFormikWizard } from './error-formik-wizard'
 export { TitleFormWizard } from './title-form-wizard'
+export { SwitchFormikWizard } from './switch-formik-wizard'
 export { classes }
