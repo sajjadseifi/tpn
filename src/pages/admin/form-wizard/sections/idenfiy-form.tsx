8ef27b451ecf0bf8e9dbb4739b9da01bc88e8420
Sajjad Seifi <s.09334497255@gmail.com>
2021-12-09 12:42:34 +0330

    complte identify section form

--- src/pages/admin/form-wizard/sections/idenfiy-form.tsx
+++ src/pages/admin/form-wizard/sections/idenfiy-form.tsx
@@ -1,13 +1,24 @@
 import React from 'react'
 import * as Yup from 'yup'
 import { regexs } from '@src/constants'
+import { InputFormikWizard } from '@src/components/ui/formik-wizard'
 
 export const idenfiySchema = Yup.object().shape({
-  email: Yup.string().email().required('این ورودی اجباری میباشد.'),
-  phone: Yup.string().matches(regexs.PHOEN, 'فریمت شماره وارد شده نادرست میباشد.').required('این ورودی اجباری میباشد.'),
-  nationalCode: Yup.string().required('این ورودی اجباری میباشد.')
+  email: Yup.string().min(8, 'حد اقل 8 حرف وارد کنید.').email().required('این ورودی اجباری میباشد.'),
+  phone: Yup.string().matches(regexs.PHONE, 'فریمت شماره وارد شده نادرست میباشد.').required('این ورودی اجباری میباشد.'),
+  nationalCode: Yup.string()
+    .min(10, 'کد ملی 10 رقمی میباشد.')
+    .max(10, 'کد ملی 10 رقمی میباشد.')
+    .matches(regexs.NATIONAL_CODE, 'فرمت کد ملی وارد شده صحیح نمیباشد.')
+    .required('این ورودی اجباری میباشد.')
 })
 
 export const IdenfiyForm = () => {
-  return <>IdenfiyForm</>
+  return (
+    <>
+      <InputFormikWizard label="ایمیل" name="email" />
+      <InputFormikWizard label="شماره همراه" name="phone" />
+      <InputFormikWizard label="کد ملی" name="nationalCode" />
+    </>
+  )
 }
