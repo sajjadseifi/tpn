55e5f2ce9a3c4da98d45485d56dbb7c8cdd318b5
Sajjad Seifi <s.09334497255@gmail.com>
2021-12-09 04:28:54 +0330

    add (feacher)  loader

--- src/components/ui/icon-button.tsx
+++ src/components/ui/icon-button.tsx
@@ -1,7 +1,7 @@
-import { WHITE } from '@src/constants/colors'
 import React, { FC } from 'react'
+import { WHITE } from '@src/constants/colors'
 import { IconBaseProps, IconType } from 'react-icons/lib'
-
+import { ImSpinner2 } from 'react-icons/im'
 interface IconButtonProps {
   title?: string
   icon?: IconType
@@ -10,26 +10,38 @@ interface IconButtonProps {
   color?: string
   outline?: boolean
   style?: any
+  loading?: boolean
+  loader?: React.FC<any>
   onClick?: () => void
 }
 export const IconButton: FC<IconButtonProps> = (props) => {
-  const { icon: Icon, title, revers, iconProps, color, onClick, outline, style, ...popo } = props
+  const {
+    icon: Icon,
+    style: styles,
+    loader,
+    title,
+    revers,
+    iconProps,
+    color,
+    onClick,
+    outline,
+    loading,
+    ...popo
+  } = props
+  let Loader = loader ? loader : ImSpinner2
+  const style = {
+    background: outline ? '' : color,
+    color: outline ? color : WHITE,
+    flexDirection: revers ? 'row-reverse' : 'row',
+    ...styles
+  }
   return (
-    <div
-      onClick={onClick}
-      className="ic"
-      style={{
-        background: outline ? '' : color,
-        color: outline ? color : WHITE,
-        display: 'flex',
-        flexDirection: revers ? 'row-reverse' : 'row',
-        alignItems: 'center',
-        ...style
-      }}
-      {...popo}
-    >
+    <div className={`ic ${loading ? 'loader' : ''}`} {...{ onClick, style, ...popo }}>
       {Icon && <Icon className="ic-icon" {...iconProps} />}
       <span className="ic-title">{title}</span>
+      <div className="loading">
+        <Loader color={WHITE} size={20} />
+      </div>
     </div>
   )
 }
