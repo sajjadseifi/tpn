c4c23086ab96858fcf449adcfb69b3314d957f39
Sajjad Seifi <s.09334497255@gmail.com>
2021-12-09 05:16:12 +0330

    build title from wizard

--- src/components/ui/formik-wizard/index.ts
+++ src/components/ui/formik-wizard/index.ts
@@ -1,4 +1,5 @@
 import classes from './form-wizard.module.css'
 export { InputFormikWizard } from './input-formik-wizard'
 export { ErrorFormikWizard } from './error-formik-wizard'
+export { TitleFormWizard } from './title-form-wizard'
 export { classes }
