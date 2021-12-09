db00e5f67fe95b726d33dbf9a4040641b9030c9d
Sajjad Seifi <s.09334497255@gmail.com>
2021-12-09 16:23:13 +0330

    build app loading for init app

---
+++ src/components/loading/app-loading/app-loading.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { classes } from '.'
+
+export const AppLoadin = () => {
+  return (
+    <div className={classes.AppLoading}>
+      <div className={classes.Cotnent}>
+        <span>Loading....</span>
+      </div>
+    </div>
+  )
+}
