152355de973b0bffb6816f5a5badedb3444f7764
Sajjad Seifi <s.09334497255@gmail.com>
2021-12-09 17:35:34 +0330

    add link on logo

--- src/components/navigation/home/components/logo.tsx
+++ src/components/navigation/home/components/logo.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import logo from '@src/assets/images/icons/tpn-logo.png'
 import { classes } from '.'
-
+import { Link } from 'react-router-dom'
 export const LogoHomeNavigation = () => (
-  <div className={classes.LogoNavBox}>
+  <Link to="/" className={classes.LogoNavBox}>
     <img className={classes.LogoImg} src={logo} alt="" />
-    <h2 className={classes.LogoTitle}>توان پردازش نگار</h2>
-  </div>
+    <h3 className={classes.LogoTitle}>توان پردازش نگار</h3>
+  </Link>
 )
