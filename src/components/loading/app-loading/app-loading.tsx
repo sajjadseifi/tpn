7eb42335fcff3729d59fc086974d14a6ebd503aa
Sajjad Seifi <s.09334497255@gmail.com>
2021-12-09 23:28:54 +0330

    fixed (issue) change to correct name app loading

--- src/components/loading/app-loading/app-loading.tsx
+++ src/components/loading/app-loading/app-loading.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { classes } from '.'
 import './app-loading.scss'
 import { SnipperKebrit } from './snipper-kebrit'
-export const AppLoadin = () => {
+export const AppLoading = () => {
   return (
     <div className={classes.AppLoading}>
       <SnipperKebrit />
