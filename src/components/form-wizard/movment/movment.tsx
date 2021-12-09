e899b84bb39c54ec61b1da1996796affce637ea4
Sajjad Seifi <s.09334497255@gmail.com>
2021-12-09 04:30:31 +0330

    use loading with submiting

--- src/components/form-wizard/movment/movment.tsx
+++ src/components/form-wizard/movment/movment.tsx
@@ -10,23 +10,19 @@ import { Partial } from '@src/types/types'
 interface MovmentProps {
   submitForm: (() => Promise<void>) & (() => Promise<any>)
   isSubmitting?: boolean
-  loading?: boolean
 }
 
-export const Movment: FC<Partial<MovmentProps>> = ({ submitForm, isSubmitting }) => {
-  console.log({ isSubmitting })
+export const Movment: FC<Partial<MovmentProps>> = ({ submitForm, isSubmitting: loading }) => {
   const dispatch = useDispatch()
   const { level, wizards } = useSelector<RootState>((state) => state.wizard) as WizardState
   const isStart = 0 === level
   const isEnd = wizards.length - 1 == level
 
   const onPrev = () => dispatch(wizardActions.prevWizard())
-  // const onNext = () => dispatch(wizardActions.nextWizard())
-  // const onSubmit = () => dispatch(wizardActions.submitedWizard())
   return (
     <div className={classes.Movment}>
       {isStart ? <div></div> : <IconButton onClick={onPrev} {...prevBtn} />}
-      <IconButton loading={isSubmitting} onClick={submitForm} {...(isEnd ? submitedBtn : nextBtn)} />
+      <IconButton loading={loading} onClick={submitForm} {...(isEnd ? submitedBtn : nextBtn)} />
     </div>
   )
 }
